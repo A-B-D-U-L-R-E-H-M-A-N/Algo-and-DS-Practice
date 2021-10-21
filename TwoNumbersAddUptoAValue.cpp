47c094ec1ce51bbb3a9c730d4c63b6ff0bc0f3ed
A-B-D-U-L-R-E-H-M-A-N <this.is.ab365@gmail.com>
2021-10-21 20:51:32 +0500

    Improved.

--- TwoNumbersAddUptoAValue.cpp
+++ TwoNumbersAddUptoAValue.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
 //Return 2 elements of arr that sum to the given value
 int * findSum(int arr[], int value, int size) {
   // Sorting function is prepended as sort(arr, low, high)
@@ -22,3 +26,27 @@ int * findSum(int arr[], int value, int size) {
     }
 	return arr; 
 } 
+
+
+int main(){
+
+  int size = 5, value = 9;
+  int arr[] = {2,4,5,7,8};
+  
+  if(size > 0){
+    int * arr2 = findSum(arr, value, size);
+    int num1 = arr2[0];
+    int num2 = arr2[1];
+
+      if((num1 + num2) != value)
+        cout << "Not Found" << endl;
+      else {
+        cout << "Number 1 = " << num1 << endl;
+        cout << "Number 2 = " << num2 << endl;
+        cout << "Sum = " << num1+num2 << endl;
+
+      }
+    } else {
+      cout << "Input Array is Empty!" << endl;
+    }
+}
