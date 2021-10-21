4e3485cbf14986ac77e1fa8e1b482111887636b1
A-B-D-U-L-R-E-H-M-A-N <this.is.ab365@gmail.com>
2021-10-21 15:27:18 +0500

    This function takes two sorted arrays and adds them into a single sorted array.

--- MergeSortedArrays.cpp
+++ MergeSortedArrays.cpp
@@ -12,8 +12,9 @@ int * mergeArrays(int arr1[], int arr2[], int arr1Size,int arr2Size)
     int arr1Index = 0, arr2Index = 0;
     int arr1finished = 0, arr2finished = 0;
 
+    // looping through the array
     for(int i=0;i<(arr1Size + arr2Size); i++){
-        if(arr1[arr1Index] < arr2[arr2Index] && arr1finished==0){
+        if(arr1[arr1Index] < arr2[arr2Index] && arr1finished==0){ // smallest element from the array if not finished.
             arr3[i] = arr1[arr1Index];
             
              if(arr1Index == arr1Size-1){
@@ -24,7 +25,7 @@ int * mergeArrays(int arr1[], int arr2[], int arr1Size,int arr2Size)
                 arr1Index++;
             }
            
-        }else if(arr2[arr2Index] <= arr1[arr1Index] && arr2finished ==0){
+        }else if(arr2[arr2Index] <= arr1[arr1Index] && arr2finished ==0){ // else the element from the other element.
             arr3[i] = arr2[arr2Index];
             
             if(arr2Index == arr2Size-1){
@@ -35,11 +36,11 @@ int * mergeArrays(int arr1[], int arr2[], int arr1Size,int arr2Size)
                 arr2Index++;
             }
             
-        }else if(arr1finished==1){
+        }else if(arr1finished==1){ // if array1 finished, copy the remaining elements from array2
             arr3[i] = arr2[arr2Index];
             arr2Index++;
-        }else{
-            arr3[i] = arr1[arr1Index];
+        }else{ // If array2 has finished, copy all the remaining elements from array1
+            arr3[i] = arr1[arr1Index]; 
             arr1Index++;
         }
     }
@@ -49,6 +50,15 @@ int * mergeArrays(int arr1[], int arr2[], int arr1Size,int arr2Size)
 
 int main(int argc, char const *argv[])
 {
-    
+    int array1[] = {1,6,9,10,15,18, 19};
+    int array2[] = {16,19,23,45,67,89};
+    int *array3 = mergeArrays(array1,array2,7,6);
+
+    cout << "The merged array: " << endl;
+    for(int i=0; i<12;i++){
+        cout << array3[i] << " ";
+    }
+
+    cout << endl;
     return 0;
 }
